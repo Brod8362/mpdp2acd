2bb942efa923a24b9e34eb3d8f513c974245997c
Brod8362 <brod8362@gmail.com>
2022-02-27 18:09:26 -0600

    Finish adding mp3 disc support

--- mpdp2acd.pl
+++ mpdp2acd.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use JSON;
 use File::Temp "tempdir";
+use File::Basename;
 
 sub Info {
     print("[\033[36;1mINFO\033[0m] $_[0]\n");
@@ -38,6 +39,10 @@ sub GenTrackTextBlock { # song URI, index
             Info("Using fallback album_artist for $_[0]\n");
             $artist = $temp;
         }
+    }
+
+    if (!defined $title) {
+        $title = $json->{"format"}{"tags"}{"TITLE"};
     }
 
     #Ensure title meta is valid
@@ -87,6 +92,20 @@ CD_TEXT {
 FILE \"$_[1].wav\" 0
 "
 }
+
+#parameter order:
+# 0: source file path 1: desired bitrate 2: dest file path
+sub ConvMP3 {
+    my $fname = fileparse($_[0]);
+    my @formats = (".mp3");
+    my $destpath = $_[2]."/".$fname.".mp3";
+    my $ffmpeg_cmd = "ffmpeg -loglevel quiet -i \"$_[0]\" -map_metadata 0 -b:a $_[1]k \"$destpath\"";
+    my $exit = system($ffmpeg_cmd);
+    if ($exit != 0) {
+        Error("Failed to convert ".$_[0]);
+    }
+}
+
 my $HOME = $ENV{"HOME"};
 my @mpd_configs = ("$HOME/.mpdconf", "$HOME/.config/mpd/mpd.conf","/etc/mpd.conf");
 sub DetermineMpdMusicDirectory {
@@ -121,6 +140,31 @@ sub DeterminePlaylistLength {
     return $total_seconds;
 }
 
+sub CommandExists {
+    my $exit = `sh -c 'command -v $_[0]'`;
+    return $exit;
+}
+
+my @required_commands = ("ffmpeg", "ffprobe", "mpc", "cdrdao");
+my @mp3_req_commands = ("cdrecord", "mkisofs");
+my $mp3_avail = 1;
+
+for (my $i = 0; $i < scalar @required_commands; $i++) {
+    my $cmd = $required_commands[$i];
+    if (!CommandExists($cmd)) {
+        Error("Cannot find required command $cmd, exiting");
+        exit 1;
+    }
+}
+
+for (my $i = 0; $i < scalar @mp3_req_commands; $i++) {
+    my $cmd = $mp3_req_commands[$i];
+    if (!CommandExists($cmd)) {
+        Warn("Cannot find command $cmd, which is necessary for MP3 format discs. Only WAV will be available.");
+        $mp3_avail = 0;
+    }
+}
+
 my $mpd_music_directory = DetermineMpdMusicDirectory();
 $mpd_music_directory =~ s/~/\$HOME/g;
 print("Enter music directory (leave blank for $mpd_music_directory):");
@@ -150,43 +194,54 @@ my $playlist_length_seconds = DeterminePlaylistLength($playlists[$selection]);
 Info("Playlist length is ".$playlist_length_seconds." seconds");
 my $cd_format_choice = "";
 my @mp3_formats = (64,96,128,196,256);
+my @valid = ();
 for (my $i = 0; $i < scalar @mp3_formats; $i++) {
     my $bitrate = $mp3_formats[$i];
     my $total_size = $playlist_length_seconds * (($bitrate)/(8*1024)); # total size in megabytes
     my $color = "\033[1m";
     my $extra = "";
-    if ($total_size > 650) {
+    if ($total_size > 650 || !$mp3_avail) {
         $color = "\033[90;1m";
         $extra = "(unavailable)";
+    } else {
+        push(@valid, ($i+1));
     }
     printf("%s%d - MP3 [%dkbps] (~%.1f MB) %s\033[0m\n", $color, $i+1, $bitrate, $total_size, $extra);
 }
 if ($playlist_length_seconds > 80*60) { #80 minutes
     printf("\033[90;1m%d - WAV (unavailable)\033[0m\n", scalar @mp3_formats+1);
 } else {
+    push(@valid, (scalar @mp3_formats+1));
     printf("\033[1m%d - WAV\033[0m\n", scalar @mp3_formats+1);
 }
 
 my $format_selection = -1;
-while ($format_selection <= 0 || $format_selection > scalar @mp3_formats+1) {
+while (!($format_selection ~~ @valid)) {
     printf("Please select an option:");
     $format_selection = <>;
+    chomp($format_selection);
 }
 $format_selection--; #move it into valid range
 
 my @songs = split("\n", `mpc playlist -f '\%file\%' '$playlists[$selection]'`);
 
-my $tmp_dir =  tempdir(CLEANUP => 1);
+my $tmp_dir = tempdir(CLEANUP => 1);
 my @cdtext_info = ();
 for (my $i = 0; $i < scalar @songs; $i++) {
     my $song = $mpd_music_directory."/".$songs[$i];
-    my $song_index = $i+1;
-    $cdtext_info[$i] = GenTrackTextBlock($song, $song_index);
-    my $ffmpeg_cmd = "ffmpeg -loglevel quiet -i \"$song\" -map_metadata 0 -ar 44100 \"$tmp_dir/$song_index.wav\"";
-    my $exit = system($ffmpeg_cmd);
+    if ($format_selection eq scalar @mp3_formats) { #for WAV
+        my $song_index = $i+1;
+        $cdtext_info[$i] = GenTrackTextBlock($song, $song_index);
+        my $ffmpeg_cmd = "ffmpeg -loglevel quiet -i \"$song\" -map_metadata 0 -ar 44100 \"$tmp_dir/$song_index.wav\"";
+        my $exit = system($ffmpeg_cmd);
+    } else { # for mp3
+        Info("Converting ".($i+1)."/".scalar @songs);
+        ConvMP3($song, $mp3_formats[$format_selection], $tmp_dir); #convert it to mp3 in the temp directory
+    }
 }
-my $final_cdtext = undef;
-my $formatted_cdtext_info = join("\n", @cdtext_info);
+
+#cd text/artist
+
 print("CD Text Title (leave blank for $playlists[$selection]):");
 my $cdtext_title = <>;
 chomp($cdtext_title);
@@ -196,7 +251,11 @@ if ($cdtext_title eq "") {
 print("CD Text Artist:");
 my $cdtext_artist = <>;
 chomp($cdtext_artist);
-$final_cdtext = 
+
+if ($format_selection eq scalar @mp3_formats) { #for WAV
+    my $final_cdtext = undef;
+    my $formatted_cdtext_info = join("\n", @cdtext_info);
+    $final_cdtext = 
 "CD_DA
 
 CD_TEXT {
@@ -209,34 +268,56 @@ CD_TEXT {
     }
 }
 
-$formatted_cdtext_info
-";
-open(FH, '>', "$tmp_dir/toc.txt");
-print FH $final_cdtext;
-close(FH);
-
-my $use_wavegain = YesNo("Would you like to use wavegain to normalize volume levels?");
-if ($use_wavegain) {
-    my $exit_code = system("cd $tmp_dir && wavegain -y $tmp_dir/*.wav 2>/dev/null");
-    if ($exit_code != 0) {
-        die("wavegain returned non-zero exit code $exit_code");
-    }
-    print("wavegain processing completed\n");
-}
-
-my $burn_cmd = "cdrdao write --device /dev/sr0 --driver generic-mmc:0x10 -v 2 --eject toc.txt";
-my $burn_immediately = YesNo("Do you want to immediately burn this data to disc? (If no, a tar archive will be generated in your home directory)");
-if ($burn_immediately) {
-    my $exit_code = system($burn_cmd);
-    if ($exit_code != 0) {
-        die("Failed to burn CD: cdrdao returned non-zero exit code $exit_code");
-    }
-} else {
-    open(FH, '>', "$tmp_dir/burn.sh");
-    print FH $burn_cmd;
-    print FH "\n# If this command fails (or if the cd-text data isn't present on the disc), try switching the driver to generic-mmc-raw instead. Using generic-mmc:0x10 worked for me with a TSSTcorp SH-224BB.";
+    $formatted_cdtext_info
+    ";
+    open(FH, '>', "$tmp_dir/toc.txt");
+    print FH $final_cdtext;
     close(FH);
-    my $tar_filename = "mpdp2acd-$playlists[$selection].tar.gz";
-    my $exit_code = system("cd $tmp_dir && tar -czf ~/$tar_filename *");
-    print("A file named $tar_filename has been placed in your home directory, run burn.sh to begin the burning process.");
+
+    my $use_wavegain = YesNo("Would you like to use wavegain to normalize volume levels?");
+    if ($use_wavegain) {
+        my $exit_code = system("cd $tmp_dir && wavegain -y $tmp_dir/*.wav 2>/dev/null");
+        if ($exit_code != 0) {
+            die("wavegain returned non-zero exit code $exit_code");
+        }
+        Info("wavegain processing completed\n");
+    }
+
+    my $burn_cmd = "cdrdao write --device /dev/sr0 --driver generic-mmc:0x10 -v 2 --eject toc.txt";
+    my $burn_immediately = YesNo("Do you want to immediately burn this data to disc? (If no, a tar archive will be generated in your home directory)");
+    if ($burn_immediately) {
+        my $exit_code = system($burn_cmd);
+        if ($exit_code != 0) {
+            die("Failed to burn CD: cdrdao returned non-zero exit code $exit_code");
+        }
+    } else {
+        open(FH, '>', "$tmp_dir/burn.sh");
+        print FH $burn_cmd;
+        print FH "\n# If this command fails (or if the cd-text data isn't present on the disc), try switching the driver to generic-mmc-raw instead. Using generic-mmc:0x10 worked for me with a TSSTcorp SH-224BB.";
+        close(FH);
+        my $tar_filename = "mpdp2acd-$playlists[$selection].tar.gz";
+        my $exit_code = system("cd $tmp_dir && tar -czf ~/$tar_filename *");
+        print("A file named $tar_filename has been placed in your home directory, run burn.sh to begin the burning process.");
+    }
+} else { #mp3 handling logic, need to use different software
+    Info("Creating CD session...");
+    my $isoname = "mpdp2acd-".$cdtext_title.".iso";
+    my $exit = system("mkisofs -V \"$cdtext_title\" -J -r -o \"$isoname\" $tmp_dir");
+    if ($exit != 0) {
+        Error("Failed to create CD session.");
+        exit 1;
+    }
+    Info("CD session generated.");
+    my $burn_now = YesNo("Would you like to burn this to disc immediately?");
+    if ($burn_now) {
+        printf("Path to CD drive [leave blank for /dev/sr0]:");
+        my $cd_dev = <>;
+        chomp($cd_dev);
+        if ($cd_dev eq "") {
+            $cd_dev = "/dev/sr0";
+        }
+
+    } else {
+        Info("An iso is available in the current directory.");
+    }
 }
